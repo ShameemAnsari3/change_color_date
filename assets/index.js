886f18d5078b14984c06be3cbb9d59749ecff24a
ShameemAnsari3 <145750221+ShameemAnsari3@users.noreply.github.com>
2023-09-25 12:32:51 +0530

    Update index.js
    add some js code animation

--- assets/index.js
+++ assets/index.js
@@ -3,5 +3,9 @@ function showDate(){
     var d = new Date();
 
     document.getElementById("dateTime").innerHTML =d;
+    document.getElementById("dateTime").style.color="#0098ff"
+    document.getElementById("dateTime").style.fontSize="2rem"
+    document.getElementById("dateTime").style.animation="sam 15s infinite"
 
+    setInterval(showDate, 1000)
 }
